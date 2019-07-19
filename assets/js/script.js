8a1e3a652642a7b15ba53d57e354665eda1665fa
denisse <daortega@uc.cl>
2019-07-19 17:54:24 -0400

    añadir estilos. tooltip e imagenes de proyectos

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,14 @@ $(function () {
     if (this.hash !== "") {
       event.preventDefault();
 
+      $('.nav-item').removeClass('active');
+      $(this).addClass('active');
+
       var gato = this.hash;
 
       $("html, body").animate({
-        scrollTop: $(gato).offset().top
-      }, 800, function () {
+        scrollTop: $(gato).offset().top - 100
+      }, 600, function () {
         window.location.hash = gato;
       });
     }
@@ -17,18 +20,21 @@ $(function () {
 
   //typeform
   var qs, js, q, s, d = document,
-      gi = d.getElementById,
-      ce = d.createElement,
-      gt = d.getElementsByTagName,
-      id = "typef_orm",
-      b = "https://embed.typeform.com/";
+    gi = d.getElementById,
+    ce = d.createElement,
+    gt = d.getElementsByTagName,
+    id = "typef_orm",
+    b = "https://embed.typeform.com/";
 
-    if (!gi.call(d, id)) {
-      js = ce.call(d,"script");
-      js.id = id;
-      js.src = b + "embed.js";
-      q = gt.call(d, "script")[0];
-      q.parentNode.insertBefore(js, q)
-    }; //fin typeform
+  if (!gi.call(d, id)) {
+    js = ce.call(d, "script");
+    js.id = id;
+    js.src = b + "embed.js";
+    q = gt.call(d, "script")[0];
+    q.parentNode.insertBefore(js, q)
+  }; //fin typeform
+
+  //activar tooltip
+  $('[data-toggle="tooltip"]').tooltip()
 
 });
